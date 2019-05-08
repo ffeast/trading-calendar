82775b9f6980f537bad62a4e1f88eed6ee9402a5
ffeast <ffeast@gmail.com>
2019-05-08 21:08:10 +0300

    + 2018

--- generator.py
+++ generator.py
@@ -13,17 +13,19 @@ def convert_to_lua(calendar, target, spaces):
     target.write('--- THIS IS AUTOMATICALLY GENERATED FILE ---\n')
     target.write('--- PLEASE REFER TO https://github.com/ffeast/trading-calendar ---\n')
     target.write('return {\n')
-    for year, months in calendar.items():
+    for year_i, year in enumerate(calendar.keys()):
+        if year_i > 0 :
+            target.write(',\n')
         target.write(' ' * spaces + '[' + year + '] = {\n')
-        for i, month in enumerate(months):
-            if i > 0:
+        for month_i, month in enumerate(calendar[year]):
+            if month_i > 0:
                 target.write(',\n')
             target.write(' ' * spaces * 2 + '{')
             target.write(', '.join(map(str, month)))
             target.write('}')
         target.write('\n')
-        target.write(' ' * spaces + '}\n')
-    target.write('}')
+        target.write(' ' * spaces + '}')
+    target.write('\n}')
     logging.info('done')
 
 
